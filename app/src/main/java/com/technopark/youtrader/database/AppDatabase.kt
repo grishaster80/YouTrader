53ba454d8684fd6c0b1dd24edb361feb07b40bdb
Adefful <adefful46@yandex.ru>
2021-11-28 20:34:28 +0300

    try again

--- app/src/main/java/com/technopark/youtrader/database/AppDatabase.kt
+++ app/src/main/java/com/technopark/youtrader/database/AppDatabase.kt
@@ -29,10 +29,10 @@ abstract class AppDatabase : RoomDatabase() {
         override fun migrate(database: SupportSQLiteDatabase) {
             database.execSQL("CREATE TABLE IF NOT EXISTS `currencyTransaction` (`id` INTEGER PRIMARY KEY AUTOINCREMENT, `price` REAL NOT NULL, `amount` REAL NOT NULL, `timestamp` TEXT DEFAULT CURRENT_TIMESTAMP, `cryptoCurrencyId` TEXT NOT NULL, FOREIGN KEY(`cryptoCurrencyId`) REFERENCES `LocalCryptoCurrencyTransaction`(`id`) ON UPDATE NO ACTION ON DELETE NO ACTION )")
             database.execSQL("CREATE TABLE IF NOT EXISTS `LocalCryptoCurrencyTransaction` (`id` TEXT NOT NULL, `symbol` TEXT NOT NULL, `name` TEXT NOT NULL, PRIMARY KEY(`id`))")
-            database.execSQL("INSERT INTO LocalCryptoCurrencyTransaction values(`1`,`BTC`,`Bitcoin`)")
-            database.execSQL("INSERT INTO currencyTransaction (price,amount,timestamp,cryptoCurrencyId) values(12.888,0.00123,`1637722187`,`1`)")
-            database.execSQL("INSERT INTO currencyTransaction (price,amount,timestamp,cryptoCurrencyId) values(6.456,-0.00023,`1619578187`,`1`)")
-            database.execSQL("INSERT INTO currencyTransaction (price,amount,timestamp,cryptoCurrencyId) values(300.55,0.0011,`1619491787`,`1`)")
+            //database.execSQL("INSERT INTO LocalCryptoCurrencyTransaction values(`1`,`BTC`,`Bitcoin`)")
+            //database.execSQL("INSERT INTO currencyTransaction (price,amount,timestamp,cryptoCurrencyId) values(12.888,0.00123,`1637722187`,`1`)")
+            //database.execSQL("INSERT INTO currencyTransaction (price,amount,timestamp,cryptoCurrencyId) values(6.456,-0.00023,`1619578187`,`1`)")
+            //database.execSQL("INSERT INTO currencyTransaction (price,amount,timestamp,cryptoCurrencyId) values(300.55,0.0011,`1619491787`,`1`)")
         }
     }
 }
