d803e229ddbcc22aafbe1819b4b7bc4b6a7a5c10
gagask <kholkovskii@mail.ru>
2021-12-01 22:54:59 +0300

    add Pin

--- app/src/main/java/com/technopark/youtrader/ui/pin/PinRegFragment.kt
+++ app/src/main/java/com/technopark/youtrader/ui/pin/PinRegFragment.kt
@@ -2,6 +2,7 @@ package com.technopark.youtrader.ui.pin
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.technopark.youtrader.R
@@ -20,9 +21,36 @@ class PinRegFragment : BaseFragment(R.layout.pin_fragment) {
         // Don't forget to call super.onViewCreated
         super.onViewCreated(view, savedInstanceState)
 
+        var pin = "undefined"
+
         with(binding) {
             label.text = getString(R.string.pin_reg_logo)
+
+            passcodeView.setPasscodeEntryListener { passcode ->
+                when(pin){
+                    "undefined" -> {
+                        pin = passcode
+                        passcodeView.clearText()
+                        label.text = "Повторите PIN"
+                    }
+                    passcode -> {
+                        setPinToPrefs(pin)
+                        Toast.makeText(activity,"PIN успешно установлен", Toast.LENGTH_SHORT).show()
+                        viewModel.navigateToProfileFragment()
+                    }
+                    else -> {
+                        passcodeView.clearText()
+                        Toast.makeText(activity,"Неправильно введен PIN", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+
+            keyboard.setPasscodeView(passcodeView)
         }
+    }
+
+    private fun setPinToPrefs(pin: String) {
+        setStringToPrefs("pin", pin)
     }
 
     companion object {
